5fe5d75f89d47d42629e754517c8dd7ca7c5633a
Viktoria Czaran <vczaran@gmail.com>
2023-06-07 17:47:30 -0400

    Completed phase IV of first_routes

--- FirstRoutes/app/controllers/users_controller.rb
+++ FirstRoutes/app/controllers/users_controller.rb
@@ -1,18 +1,46 @@
 class UsersController < ApplicationController
 
     def index
-      users = User.all
+      users = User.all.order(:id)
       render json: users
     end
 
-    def create
-      user = User.new(params.require(:user).permit(:name, :email))
-      user.save!
-      render json: user
+    def show
+        user = User.find(params[:id])
+        render json: user
     end
 
-    def show
-        render json: params
+    def create
+      user = User.new(user_params)
+      if user.save
+        render json: user
+      else
+        render json: user.errors.full_messages, status: :unprocessable_entity
+      end
     end
+
+    def update
+        user = User.find(params[:id])
+
+        if user.update(user_params)
+            redirect_to user_url(user)
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        redirect_to users_url
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end
+
   
+
 end
